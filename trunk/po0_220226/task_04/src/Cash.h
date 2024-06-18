fe0dddb55bf3c06498bf31e4d7cedac71eaa481d
Victor Yrman <vitiayrashevich1256@gmail.com>
2024-06-19 00:21:33 +0300

    fix cash

--- trunk/po0_220226/task_04/src/Cash.h
+++ trunk/po0_220226/task_04/src/Cash.h
@@ -12,10 +12,33 @@ public:
 
     Cash& operator=(const Cash& a) = default;
     bool operator==(const Cash& a) const;
-    Cash operator+(const Cash& other) const;
+    
+    friend Cash Cash::operator+(const Cash& other) const
+    {
+        int64_t resultRubles = rubles + other.rubles;
+        int32_t resultCopecks = copecks + other.copecks;
+        if (resultCopecks >= 100)
+        {
+            resultRubles += resultCopecks / 100;
+            resultCopecks = resultCopecks % 100;
+        }
+        return Cash(static_cast<double>(resultRubles) + static_cast<double>(resultCopecks) / 100.0);
+    }
 
-    friend std::ostream& operator<<(std::ostream& out, const Cash& a);
-    friend std::istream& operator>>(std::istream& in, Cash& a);
+    friend std::ostream& operator<<(std::ostream& out, const Cash& a)
+    {
+        out << a.GetRubles() << "," << a.GetCopecks() << std::endl;
+        return out;
+    }
+
+    friend std::istream& operator>>(std::istream& in, Cash& a)
+    {
+        std::cout << "Enter amount of rubles" << std::endl;
+        in >> a.rubles;
+        std::cout << "Enter amount of copecks" << std::endl;
+        in >> a.copecks;
+        return in;
+    }
 
     inline int32_t GetCopecks() const { return copecks; };
     inline int64_t GetRubles() const { return rubles; };
